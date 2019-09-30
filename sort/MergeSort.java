ab024df5815b6a0934556f44bfec7fff67889955
Ken <kensong96@gmail.com>
2019-09-30 15:07:50 -0400

    19.sep.30

--- sort/MergeSort.java
+++ sort/MergeSort.java
@@ -16,22 +16,18 @@ public class MergeSort {
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        /* Create temp arrays */
+        /* Create temp arrays and Copy data */
         int L[] = new int[n1];
         int R[] = new int[n2];
-
-        /* Copy data to temp arrays */
         for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
 
-        /* Merge the temp arrays */
-
         // Initial indexes of first and second subarrays
         int i = 0, j = 0;
 
-        // Initial index of merged subarry array
+        // Initial index of merged subarry array and Merge*/
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
